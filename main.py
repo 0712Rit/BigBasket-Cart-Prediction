1a2a9119f414660e7714f82db767927ba4438a44
RITAM MALLICK <89799813+0712Rit@users.noreply.github.com>
2025-06-17 14:19:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ def load_data(path="BigBasket.csv"):
     df.dropna(subset=["product", "category", "brand"], inplace=True)
     df = df[["product", "category", "brand"]]
     df.drop_duplicates(inplace=True)
+    df.reset_index(drop=True, inplace=True)  # Ensures compatibility with similarity matrix
     return df
-
-
